b45ac2f9b40cd98fd628a4b919dcfda981534465
Syed Azbar Hasnain Ali <109806090+Hasnain11222@users.noreply.github.com>
2022-10-01 10:59:19 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var a = 67.89877;
+document.write(a + "<br>");
+var b = Math.round(a);
+document.write(b);
+
+
+document.write(Math.round(a) + "<br>");
+
+
+
+var b = 45.3;
+document.write ("this is  a ceill function " + Math.ceil(b) + "<br>")
+
+
+document.write ("this is  a  floor function " + Math.floor(b) +"<br>")
+
+
+document.write ("this is  a SQRT function " + Math.round(Math.sqrt(b)) + "<br>" + "this is SQRT with round" + Math.round(b) + "<br>")
+
+
+
+
+
+var c = Math.random()*100
+document.write("math.random fuction : " + c + "this is a floor")
